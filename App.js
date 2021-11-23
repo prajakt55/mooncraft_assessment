dad00a18743981b576206797496e76908a6bf407
prajakt chatur <85507121+prajakt55@users.noreply.github.com>
2021-11-23 16:23:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     
+
+        <div className="left">
+        <p >id: 1</p>
+        <p >Name: Ravi</p>
+        <p >Email: Ravi@gmail.com</p>
+        <p>Gender: Male</p>
+        {/* <p> About me: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum eros vitae enim mollis molestie. Morbi malesuada laoreet felis fringilla dignissim. Cras gravida quis augue et pellentesque. Integer fermentum ante in hendrerit tincidunt</p> */}
+        </div>
+
+        <div className="right">
+        <p> Joining date :24 September 2019 </p>
+        <p>Location: Banglore</p>
+        <p>Department : Developement</p>
+        </div>
+
+        <div>
+        </div>
+
+
+        
+      
+    </div>
+  );
+}
